e916fae6f9a6354aafd0e33c6744dc680f75008b
Volodymyr Tsymbalistyi <wowa.zym@gmail.com>
2016-06-02 10:36:13 +0300

    Updated db Schema

--- Database/Common/0.1/Schema/Create/011_Table_Article.sql
+++ Database/Common/0.1/Schema/Create/011_Table_Article.sql
@@ -2,15 +2,11 @@ CREATE TABLE `Article`
 (
     Id INT AUTO_INCREMENT PRIMARY KEY,
     Name NVARCHAR(256) NOT NULL,
-    ProblemId INT NOT NULL,
     AuthorId INT NOT NULL,
     Solution MEDIUMTEXT NOT NULL,
     CreatedDateTime DATETIME NOT NULL
 );
 
-ALTER TABLE `Article`
-ADD CONSTRAINT fk_Article_Problem
-FOREIGN KEY (`ProblemId`) REFERENCES `Problem` (`Id`);
 
 ALTER TABLE `Article`
 ADD CONSTRAINT fk_Article_User
